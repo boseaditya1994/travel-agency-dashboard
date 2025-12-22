98572d12d4f67219f94ffa919fccccede397d17d
boseaditya1994 <boseaditya1994#gmail.com>
2025-12-22 17:01:23 +0530

    Sentry Installation and Setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import {sentryReactRouter, type SentryReactRouterBuildOptions} from "@sentry/react-router";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  ssr: {
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "js-mastery-yx",
+  project: "travel-agency",
+  // An auth token is required for uploading source maps.
+  authToken: "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"
+  // ...
+};
+
+export default defineConfig(config => {
+  return {
+    plugins: [tailwindcss(), tsconfigPaths(), reactRouter(), sentryReactRouter(sentryConfig, config)],
+    sentryConfig,
+    ssr: {
       noExternal: [/@syncfusion/]
     }
+  };
 });
